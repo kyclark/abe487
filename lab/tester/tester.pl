cf5770841b0f0e79ee7256ee734459d54681a651
Ken Youens-Clark <kyclark@gmail.com>
2015-10-14 08:41:25 -0700

    added lib dir for HPC

--- lab/tester/tester.pl
+++ lab/tester/tester.pl
@@ -3,13 +3,14 @@
 use strict;
 use warnings;
 use feature 'say';
+use lib '/rsgrps/bhurwitz/hurwitzlab/perl5/lib/perl5';
 use autodie;
 use Data::Dumper;
 use Getopt::Long;
 use File::Spec::Functions;
 use Pod::Usage;
-use Test::Script::Run qw'is_script_output';
 use Test::More 'no_plan';
+use Test::Script::Run qw'is_script_output';
 
 main();
 

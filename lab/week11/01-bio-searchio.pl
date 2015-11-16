703e18dbcf77aa0a1dc321e862df9e6ee73c01c3
Ken Youens-Clark <kyclark@gmail.com>
2015-11-16 11:56:38 -0700

    my solution

---
+++ lab/week11/01-bio-searchio.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+
+use strict;
+use feature 'say';
+use File::Basename;
+use Bio::SearchIO;
+
+my $blast_out = shift or die sprintf("Usage: %s blast.out\n", basename($0));
+my $min       = 1e-50;
+my $searchio  = Bio::SearchIO->new( 
+    -format   => 'blast',
+    -file     => $blast_out,
+);
+
+say join "\t", qw[query hit evalue];
+while (my $result = $searchio->next_result ) {
+    while (my $hit = $result->next_hit) {
+        while (my $hsp = $hit->next_hsp) {
+            if ($hsp->significance <= $min) {
+                say join "\t", 
+                    $hsp->query_string, $hsp->hit_string, $hsp->evalue;
+            }
+        }
+    }
+}
